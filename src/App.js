2ca4385b6f94420e52e95c7c29a6e87d054410a3
unknown <izzy.zammil@gmail.com>
2022-05-18 15:13:01 +0700

    Configure React Router Dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import Product from "./components/Product";
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
+      </Routes>
     </>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 function Product() {
   const [data, setData] = useState([]);
